2b731aa4ad5a480817a71e833436e157979a1211
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2024-05-28 13:08:14 -0700

    auto commit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""Function that prints top ten hot posts for a given subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Gets top ten posts in subreddit
+       Args:
+           subreddit (str): name of subreddit
+    """
+    base_url = 'https://api.reddit.com/r/'
+    headers = {'User-Agent': 'my-app/0.0.1'}
+    response = requests.get(
+        '{}{}/hot?limit=10'.format(
+            base_url, subreddit), headers=headers, allow_redirects=False)
+
     if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+        print('None')
+    else:
+        hot_dict = response.json()
+        if len(hot_dict['data']['children']) == 0:
+            print('None')
+        else:
+            for d in hot_dict['data']['children']:
+                print(d['data']['title'])
