201e136b5a6ffcfb7398f35a05040d813374e349
abderrahman bouichou <abderrahman4bouichou@gmail.com>
2024-05-28 13:04:56 -0700

    auto commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "MyRedditApp/0.1 by YourUsername"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        try:
+            data = response.json()
+            subscribers = data['data']['subscribers']
+            return subscribers
+        except (ValueError, KeyError):
+            return 0
     else:
         return 0
